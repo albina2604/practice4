ed6db8e1acc6946a0b1bfde2d95ac9074274a84d
albina2604 <albinamilshyna2604@gmail.com>
2022-10-29 15:43:08 +0300

    Update task1.py

--- task1.py
+++ task1.py
@@ -1,4 +1,17 @@
-with open('task1.txt', 'w') as file:
+def task1():
+    '''
+    Решение к задаче 1
+    Arg:
+    n - числа, введенные пользователем
+
+    Result:
+    новый файл - 'task1.txt' 
+    '''
     n = list(map(float, input('Ведите числа - ').split()))
-    for i in range(len(n)):
-        file.write(str(n[i]) + '\n')
+    with open('task1.txt', 'w', encoding='UTF-8') as file:
+        for i in range(len(n)):
+            file.write(str(n[i]) + '\n')
+        return n
+
+
+task1()
